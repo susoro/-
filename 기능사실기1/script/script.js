3bca8f56fffbb09f71e6260611f77f3ecf72b3a0
jenn <susoro567@gmail.com>
2021-03-27 23:19:20 +0900

    js

---
+++ 기능사실기1/script/script.js
@@ -0,0 +1,19 @@
+//js
+
+// navi 슬라이드다운
+jQuery(document).ready(function(){ 
+    $('.navi>li').mouseover(function(){
+        $(this).find('.submenu').stop().slideDown(500);
+    }).mouseout(function(){
+        $(this).find('submenu').stop().slideUp(500);
+    });
+
+    $('.imgslide a:gt(0)').hide();
+    setInterval(function(){
+        $('.imgslide a:first-child').fadeOut()
+        .next('a').fadeIn()
+        .end().appendTo('imgslide');
+    },3000);
+
+    
+});
